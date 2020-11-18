85f5f78075641e639868976a64695f6c1bd5823c
Lucas <lucaspedrazoli@gmail.com>
2020-11-18 17:49:37 -0300

    add combine user choice and value example

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -94,7 +94,18 @@ example(of: "combine latest") {
   right.onCompleted()
 }
 
+example(of: "combine user choice and value") {
+  let choice: Observable<DateFormatter.Style> = Observable.of(.short, .long)
+  let dates = Observable.of(Date())
 
+  let observable = Observable.combineLatest(choice, dates) { format, when -> String in
+    let formatter = DateFormatter()
+    formatter.dateStyle = format
+    return formatter.string(from: when)
+  }
+
+  _ = observable.subscribe(onNext: { print($0) })
+}
 
 
 
